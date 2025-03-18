2b989510fccf229e0591345b889d927ab81719b8
qeftser <qeftser1@gmail.com>
2025-03-18 16:02:27 -0600

    vectornav on jetson

--- src/sensors/jetson/vectornav_ws/src/vectornav-ros2/build/vectornav/CMakeFiles/vn_sensor_msgs.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for vn_sensor_msgs.
-# This may be replaced when dependencies are built.
--- src/sensors/jetson/vectornav_ws/vectornav_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/qeftser/vip/vectornav/vectornav_msgs__py
-# Build directory: /home/qeftser/vip/vectornav/vectornav_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
