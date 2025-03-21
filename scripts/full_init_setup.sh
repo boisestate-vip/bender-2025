2632b9b6d72161c7a67e4b763477fddd4feb41c3
qeftser <qeftser1@gmail.com>
2025-03-20 23:27:51 -0600

    fix init script

--- scripts/full_init_setup.sh
+++ scripts/full_init_setup.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 sudo echo "root aquired"
 
 sudo bash $SCRIPT_DIR/bender_init.sh $1
-bash $SCRIPT_DIR/setup.sh
+. $SCRIPT_DIR/setup.sh
 bash $SCRIPT_DIR/setup_and_test_packages.sh
