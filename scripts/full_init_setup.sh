4b91d71f39e9f18a5f4614dcfbfe18096631170f
qeftser <qeftser1@gmail.com>
2025-03-21 13:30:41 +0900

    full setup script

---
+++ scripts/full_init_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+sudo echo "root aquired"
+
+sudo bash $SCRIPT_DIR/bender_init.sh $1
+bash $SCRIPT_DIR/setup.sh
+bash $SCRIPT_DIR/setup_and_test_packages.sh
